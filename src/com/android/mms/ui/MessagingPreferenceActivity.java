e842b680cb36d72036f773b68f55cd7b4b340aa1
Liquid Smooth <liquid0624@gmail.com>
2012-04-16 03:34:40 -0400

    Fix Some Sms/Mms Derpage

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -61,8 +61,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String SMS_SPLIT_MESSAGE        = "pref_key_sms_split_160";
     public static final String SMS_SPLIT_COUNTER        = "pref_key_sms_split_counter";
     public static final String NOTIFICATION_ENABLED     = "pref_key_enable_notifications";
-    public static final String SMS_SPLIT_MESSAGE        = "pref_key_sms_split_160";
-    public static final String SMS_SPLIT_COUNTER        = "pref_key_sms_split_counter";
     public static final String NOTIFICATION_VIBRATE     = "pref_key_vibrate";
     public static final String NOTIFICATION_VIBRATE_WHEN= "pref_key_vibrateWhen";
     public static final String NOTIFICATION_RINGTONE    = "pref_key_ringtone";
@@ -82,7 +80,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String NOTIFICATION_VIBRATE_PATTERN = "pref_key_mms_notification_vibrate_pattern";
     public static final String NOTIFICATION_VIBRATE_PATTERN_CUSTOM = "pref_key_mms_notification_vibrate_pattern_custom";
     public static final String NOTIFICATION_VIBRATE_CALL ="pre_key_mms_notification_vibrate_call";
-    public static final String SENT_TIMESTAMP           = "pref_key_sms_sent_time";
+    public static final String SMS_SENT_TIMESTAMP           = "pref_key_sms_sent_time";
 
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
