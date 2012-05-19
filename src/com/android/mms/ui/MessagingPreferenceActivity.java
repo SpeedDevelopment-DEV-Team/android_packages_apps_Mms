c9392f937613a8e1d22513c410677a39ca4d59ea
Austrie <austrieshane@gmail.com>
2012-05-19 01:38:06 -0500

    Revert "Revert "Plz Test locally before commiting or use exp branches...Revert "Mms Signature Support..."""
    This reverts commit 814a355b4f63157a539361be36a8a31fb48baa7c.

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -76,8 +76,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String ENABLE_EMOJIS = "pref_key_enable_emojis";
     public static final String FULL_TIMESTAMP           = "pref_key_mms_full_timestamp";
     public static final String SENT_TIMESTAMP           = "pref_key_mms_use_sent_timestamp";
-    public static final String SIGNATURE                = "pref_key_mms_signature";
-    public static final String SIGNATURE_AUTO_APPEND    = "pref_key_mms_signature_auto_append";
     public static final String NOTIFICATION_VIBRATE_PATTERN = "pref_key_mms_notification_vibrate_pattern";
     public static final String NOTIFICATION_VIBRATE_PATTERN_CUSTOM = "pref_key_mms_notification_vibrate_pattern_custom";
     public static final String NOTIFICATION_VIBRATE_CALL ="pre_key_mms_notification_vibrate_call";
